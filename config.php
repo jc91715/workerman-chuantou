0f7a31371c27309c5fc47306a848825b6a4a6e24
jcc <370234915@qq.com>
2021-04-05 19:39:04 +0800

    index

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'secret'=>'abcdefghijk',//连接密钥 11位
+    'secret'=>'abcdefghijl',//连接密钥 11位
     'server_ip'=>'workerman-chuantou.jc91715.top',//配置外网访问的那个连接，有端口的话带端口
     'server_port'=>'8483',//服务器监听端口
-    'local_address' => '127.0.0.1:80',//本地代理的地址
+    'local_address' => 'nginx:8483',//本地代理的地址
     'client_worker_count'=> 5,//本地开启多少个进程
     'client_close_re_connect'=> true,//本地开启断线重连
     'after_re_connect'=> 2//每几秒重连
